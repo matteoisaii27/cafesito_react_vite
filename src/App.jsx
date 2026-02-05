d6e639d1d0d9ea80ee3c91a6202f31547ea95444
mateopelonpelorico <232826707+mateopelonpelorico@users.noreply.github.com>
2026-02-04 19:30:46 -0500

    Implementación de marcadores en el mapa

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './App.css'
 
-const INITIAL_CENTER = [-86.8515, 21.1619] // Cancún
+import Dashboard from './components/Dashboard'
+import Quotes from './components/Quotes'
+
+
+const INITIAL_CENTER = [-86.8515, 21.1619] 
 const INITIAL_ZOOM = 12
 
 function App() {
@@ -32,7 +36,42 @@ function App() {
       zoom: zoom
     })
 
-    // ESCUCHAR MOVIMIENTO DEL MAPA
+
+
+
+    //MARCADORES EN EL MAPA
+
+    const marker1 = new mapboxgl.Marker({color: 'turquoise'})
+    .setLngLat([ -86.84698817661992, 21.049591777377092])
+    .addTo(mapRef.current);
+
+
+    const marker2 = new mapboxgl.Marker({color: 'black'})
+    .setLngLat([ -86.8234021593428, 21.200617037493625])
+    .addTo(mapRef.current);
+
+
+    const marker3 = new mapboxgl.Marker({color: 'orange'})
+    .setLngLat([-86.90656000161405, 21.182906762438783])
+    .addTo(mapRef.current);
+
+     const marker4 = new mapboxgl.Marker({color: 'green'})
+    .setLngLat([-86.92788226690791, 21.16090723975761])
+    .addTo(mapRef.current);
+
+    const marker5 = new mapboxgl.Marker({color: '#1064b8'})
+    .setLngLat([ -86.83548703235746, 21.138525125714963])
+    .addTo(mapRef.current);
+
+    const marker6 = new mapboxgl.Marker({color: 'purple'})
+    .setLngLat([ -86.84750142701677, 21.05069444158195])
+    .addTo(mapRef.current);
+
+
+
+
+
+
     mapRef.current.on('move', () => {
 
       const mapCenter = mapRef.current.getCenter()
@@ -50,6 +89,11 @@ function App() {
 
   return (
   <>
+    <h1>Bienvenidos</h1>
+
+    <Dashboard />
+    <Quotes />
+
     <div className="sidebar">
       Longitude: {center[0].toFixed(4)} | 
       Latitude: {center[1].toFixed(4)} | 
